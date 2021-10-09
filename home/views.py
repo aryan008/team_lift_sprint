b9e03ea38c49697d745dd68ed45953ab71f157f1
aryan008 <adam.s.ryan@pwc.com>
2021-10-09 15:26:28 +0000

    Add: narrative for faq.html created

--- home/views.py
+++ home/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "home/index.html")
+
 
 def contact(request):
     return render(request, "home/contact.html")
 
+
 def faq(request):
     return render(request, "home/faq.html")
 
+
 def shipping_info(request):
     return render(request, "home/shipping_info.html")
+
 
 def about_us(request):
     return render(request, "home/about_us.html")
