22217fc295b4640c0b794a109bf215fda342953e
aryan008 <adam.s.ryan@pwc.com>
2021-10-12 12:07:52 +0000

    Add: create the models Product and Category in the products app and register these models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    AREA = (
+        ('individual', 'Individual'),
+        ('team', 'Team'),
+        ('universal', 'Universal'),
+        )
+    
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    area = models.CharField(max_length=200, null=True, choices=AREA)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
