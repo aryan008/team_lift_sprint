e7e5df393a763bff1edb55a399337f9fd11d3c68
aryan008 <adam.s.ryan@pwc.com>
2021-10-21 17:18:56 +0000

    Add: setup of category views in main-nav and related in views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -9,23 +9,34 @@ from .models import Product
 def all_products(request):
     """ A view to show all products including search queries """
     products = Product.objects.all()
-    # set initial query to None
+    # set initial query and categories to None
     query = None
+    categories = None
 
     if request.GET:
+        # set up category dropdown for navbar
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # set up search request for search bar
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
             # set up queries variable to handle name or description
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
         }
     return render(request, "products/products.html", context)
 
