581a1d4d9d22e3fbdd6a8d030093bbd26d486205
aryan008 <adam.s.ryan@pwc.com>
2021-10-30 14:28:11 +0000

    Add: set up the boilerplate toasts in base.html and templates

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 
 def view_bag(request):
     """ Render the shopping bag """
+    
     return render(request, "bag/bag.html")
 
 
 def add_to_bag(request, item_id):
     """ Add item and quantity to the shopping bag """
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,6 +23,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
