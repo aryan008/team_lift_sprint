45a1be37e89abf1d391164a2093605f282ed2108
aryan008 <adam.s.ryan@pwc.com>
2021-12-11 10:59:22 +0000

    Add: stripe js and form submission handling

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "No bag items present!")
         return redirect(reverse('products'))
+    
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, "Stripe public key is missing. Adam fix!")
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': "pk_test_51JM7mPDe72c6JjEfoZgon625e1JTEK1nlpZgMqySjU0cWxN0T6qwTf7o5HHPdFr9js91ywP1dViwnCW9pJXYYRZf00avZVLRdD",
-        'client_secret': "test client secret",
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
