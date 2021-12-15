71bcb34272c8b662ce77eff34caf9be8582ec18c
aryan008 <adam.s.ryan@pwc.com>
2021-12-15 14:40:23 +0000

    Add: create forms.py for site admin and class ProductForm

--- team_lift_sprint/settings.py
+++ team_lift_sprint/settings.py
@@ -116,7 +116,7 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'team_lift_sprint.wsgi.application'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-amber-dragonfly-uiq2vm6p.ws-eu23.gitpod.io',
+    'https://8000-white-donkey-3pkfraks.ws-eu23.gitpod.io',
     ]
 
 # Database
