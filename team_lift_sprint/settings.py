a1beaa481ceea35a1f30d9e2a6a42d8e59d6a939
aryan008 <adam.s.ryan@pwc.com>
2021-12-21 11:05:52 +0000

    Deploy: add secret key to heroku and debug setting

--- team_lift_sprint/settings.py
+++ team_lift_sprint/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+vi^7n&#z(ix%*%9!l&kvvo#x#t&b14eb4c1m6)#*60lc8^g9g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
