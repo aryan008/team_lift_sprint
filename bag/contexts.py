0990be63fdf17ab5e9139cf7f8a2d75a5559aa49
aryan008 <adam.s.ryan@pwc.com>
2021-12-26 16:24:12 +0000

    Docs: add documentation to bag app python files

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,10 +9,12 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    # Get bag if it exists or else initialise it
     bag = request.session.get('bag', {})
-
+    # For loop to get the total price, product sum and savings made if on sale
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
+        # If statement for products on sale
         if product.on_sale is True:
             total += quantity * Decimal(
                 product.price * (
@@ -26,11 +28,13 @@ def bag_contents(request):
                 product.discount_on_product / 100)).quantize(
                         Decimal('0.00'))
         else:
+            # Else block if product not on sale
             total += quantity * product.price
             product_total = quantity * product.price
             product_saving = 0
 
         product_count += quantity
+        # Appendation of above items to initialised/ready bag
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
@@ -39,6 +43,7 @@ def bag_contents(request):
             'product_saving': product_saving,
         })
 
+    # If statement to determine if customer is entitled to free shipping
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect,\
+    reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -7,26 +8,34 @@ from products.models import Product
 
 def view_bag(request):
     """ Render the shopping bag """
-    
+
     return render(request, "bag/bag.html")
 
 
 def add_to_bag(request, item_id):
     """ Add item and quantity to the shopping bag """
 
+    # Get the product by the item ID
     product = get_object_or_404(Product, pk=item_id)
+    # Integer request to get the quantity
     quantity = int(request.POST.get('quantity'))
+    # Grab the form redirect URL
     redirect_url = request.POST.get('redirect_url')
+    # Get the bag if it exists or initialise if not
     bag = request.session.get('bag', {})
 
+    # If theres a key in the bag that matches the item ID
     if item_id in list(bag.keys()):
+        # Increment the item ID quanitity to quantity
         bag[item_id] += quantity
         messages.success(
             request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
+        # The quantity is created as first instance
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
 
+    # Overwrite the session variable with bag and redirect
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -38,11 +47,13 @@ def adjust_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
+    # If quantity is above 0 set the item quantity
     if quantity > 0:
         bag[item_id] = quantity
         messages.success(
             request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
+        # Remove the item from the bag
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
 
@@ -56,11 +67,13 @@ def remove_from_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
 
     try:
+        # Try and remove the item from the bag and return status 200 response
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        # Error message that cannot remove item and 500 response
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
